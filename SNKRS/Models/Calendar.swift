d95ceec8a77a57884488c5967cf037ef517b151c
Emanuelle Moco <emanuellesmoco@gmail.com>
2022-02-18 23:50:08 -0300

    first commit

---
+++ SNKRS/Models/Calendar.swift
@@ -0,0 +1,13 @@
+//
+//  Calendar.swift
+//  SNKRS
+//
+//  Created by Emanuelle Moço on 18/02/22.
+//
+
+import Foundation
+
+struct Calendar {
+    let title: String //Nome do tenis
+    let date: String  // Data de lançamento
+}
---
+++ SNKRS/Views/CalendarCell.swift
@@ -0,0 +1,23 @@
+//
+//  CalendarCell.swift
+//  SNKRS
+//
+//  Created by Emanuelle Moço on 18/02/22.
+//
+
+import UIKit
+
+class CalendarCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
