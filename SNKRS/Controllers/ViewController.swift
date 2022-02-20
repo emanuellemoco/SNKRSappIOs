d323683955a6b261f57a67b6f5ffe8e3f10429c5
Emanuelle Moco <emanuellesmoco@gmail.com>
2022-02-20 01:00:48 -0300

    alert working

--- SNKRS/Controllers/ViewController.swift
+++ SNKRS/Controllers/ViewController.swift
@@ -5,15 +5,51 @@
 //  Created by Emanuelle Moço on 18/02/22.
 //
 
+import SCLAlertView
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    private let table = UITableView()
+    @IBOutlet weak var emailTextField: UITextField!
+  
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+//        table.delegate = self
+//        table.dataSource = self
     }
 
+//    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+//        return 10
+//    }
+//
+//    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = UITableViewCell()
+//        cell.textLabel?.tect
+//    }
+    @IBAction func sendButton(_ sender: UIButton) {
+        let emailReg: String = "[A-Z0-9a-z._%+-]+@[A-Za-z0-9.-]+\\.[A-Za-z]{2,4}"
+        let emailTest: NSPredicate = NSPredicate(format: "SELF MATCHES %@", emailReg)
+        
+        if emailTextField.text == "" {
+            SCLAlertView().showInfo("Atenção", subTitle: "Campo vazio")
 
+        }
+        else if emailTest.evaluate(with: emailTextField.text!) == false {
+            SCLAlertView().showError("Erro", subTitle: "Digite um email valido")
+        }
+        else {
+        SCLAlertView().showSuccess("Sucesso" , subTitle: "Agora voce vai saber de todas as novidades")
+            emailTextField.text = ""
+        }
+  
+
+
+        
+    
+    
+    
+    }
 }
 
